90f6d616dd030c024abcd606e14a861ca24fef80
SebastianToso <116979070+SebastianToso@users.noreply.github.com>
2024-10-08 10:50:27 -0300

    Update Main.java

--- Ferreteria_La_Nueva/src/main/java/org/Ferreteria_La_Nueva/Main.java
+++ Ferreteria_La_Nueva/src/main/java/org/Ferreteria_La_Nueva/Main.java
@@ -3,13 +3,15 @@ package org.Ferreteria_La_Nueva;
 import modelos.Usuarios;
 import modelos.Productos;
 import servicios.CrearUsuario;
-import servicios.CrearUsuario;
 import servicios.Login;
 import conexionDB.ConexionBD;
 
 public class Main {
     public static void main(String[] args) {
 
+        ConexionBD miConexion = new ConexionBD();
+        
+        miConexion.cerrarConexion();
 
     }
 }
