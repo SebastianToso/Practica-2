9b4d16baa0207f5563a96df5074bd79edf1e2747
SebastianToso <116979070+SebastianToso@users.noreply.github.com>
2024-10-15 16:16:42 -0300

    Delete Ferreteria_La_Nueva directory
    Se modifica.

--- Ferreteria_La_Nueva/src/main/java/servicios/CrearProducto.java
+++
@@ -1,8 +0,0 @@
-package servicios;
-
-import modelos.Productos;
-import modelos.Usuarios;
-
-public class CrearProducto {
-    Productos producto = new Productos(1,"Martillo", 50, "Stanley");
-}
--- Ferreteria_La_Nueva/src/main/java/servicios/CrearUsuario.java
+++
@@ -1,8 +0,0 @@
-package servicios;
-
-import modelos.Usuarios;
-
-public class CrearUsuario {
-
-    Usuarios usuariosAdmin = new Usuarios("sebastian", "23232323", "San Martín 356", "ferreteria001");
-}
